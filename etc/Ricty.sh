86842fb56e40e35968cbf1c65ae3e8acc4ef85bb
Yoshiaki Fujimori <info@2244.jp>
2014-07-13 17:08:09 +0900

    install Ricty

---
+++ etc/Ricty.sh
@@ -0,0 +1,5 @@
+# https://github.com/sanemat/homebrew-font
+
+$ brew install https://raw.github.com/sanemat/homebrew-font/master/ricty.rb
+$ cp -f /PATH/TO/RICTY/fonts/Ricty*.ttf ~/Library/Fonts/
+$ fc-cache -vf
