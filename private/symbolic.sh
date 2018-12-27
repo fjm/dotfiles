b1f9c730f31054b30516c6e3d6f5f27f5a5549a7
Yoshiaki Fujimori <info@2244.jp>
2018-12-27 21:14:46 +0900

    不要なsymlink削除

--- private/symbolic.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-# Dashboard Widget - 複数間Macで設定共有
-if [ ! -e ~/Library/Widgets ]; then
-  ln -s ~/Dropbox/_sync/symbolic/Users/Library/Widgets/ ~/Library/Widgets
-  echo 'ln -s Widgets'
-fi
